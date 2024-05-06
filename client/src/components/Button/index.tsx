b311c80616ad4acedf51f667469cf22fa31f190d
techuila <axlcuyugan05@gmail.com>
2024-05-07 05:12:21 +0800

    feat: added spinner on button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,13 +1,33 @@
 import { ButtonHTMLAttributes } from 'react';
+import { motion } from 'framer-motion';
+import { TailSpin } from 'react-loader-spinner';
 import cx from 'clsx';
 
 import styles from './index.module.css';
 
 type ButtonProps = {
 	variant: 'primary' | 'secondary';
+	isLoading?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ variant, className, children, ...rest }: ButtonProps) {
+const spinner = {
+	hiddden: {
+		width: 0,
+		marginRight: 0
+	},
+	visible: {
+		width: 20,
+		marginRight: 8
+	}
+};
+
+export function Button({
+	variant,
+	className,
+	isLoading,
+	children,
+	...rest
+}: ButtonProps) {
 	const variantStyles = styles[variant];
 
 	return (
@@ -15,6 +35,20 @@ export function Button({ variant, className, children, ...rest }: ButtonProps) {
 			{...rest}
 			className={cx(styles.container, variantStyles, className)}
 		>
+			<motion.div
+				initial='hiddden'
+				animate={isLoading ? 'visible' : 'hidden'}
+				variants={spinner}
+			>
+				<TailSpin
+					visible={true}
+					height='20'
+					width='20'
+					color='#000000'
+					ariaLabel='tail-spin-loading'
+					radius='1'
+				/>
+			</motion.div>
 			{children}
 		</button>
 	);
--- client/src/pages/Bingo/index.tsx
+++ client/src/pages/Bingo/index.tsx
@@ -4,8 +4,10 @@ import styles from './index.module.css';
 
 export function Bingo() {
 	return (
-		<div className={styles.mainContainer}>
-			<Grid rows={5} columns={5} />
+		<div className={styles.container}>
+			<div className={styles.mainContainer}>
+				<Grid rows={5} columns={5} />
+			</div>
 		</div>
 	);
 }
