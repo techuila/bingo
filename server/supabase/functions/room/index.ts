07715f0786611d27c447b1b61750edd557a146e3
techuila <axlcuyugan05@gmail.com>
2024-05-06 09:09:55 +0800

    fix: option method

--- server/supabase/functions/room/index.ts
+++ server/supabase/functions/room/index.ts
@@ -9,10 +9,12 @@ import {
 	createClient,
 	SupabaseClient
 } from 'https://esm.sh/@supabase/supabase-js@2.7.1';
+import { verifyKey } from 'https://esm.sh/@unkey/api';
 
 const corsHeaders = {
 	'Access-Control-Allow-Origin': '*',
-	'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey',
+	'Access-Control-Allow-Headers':
+		'authorization, x-client-info, apikey, content-type, x-api-key',
 	'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE'
 };
 
@@ -22,9 +24,19 @@ async function getRoom(supabaseClient: SupabaseClient, id: string) {
 		.select('id')
 		.eq('room_id', id)
 		.eq('is_active', true);
-	if (error) throw error;
+	if (error) {
+		return new Response(JSON.stringify({ error: error.message }), {
+			headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+			status: 500
+		});
+	}
 
-	if (data.length === 0) throw new Error('Room not found');
+	if (data.length === 0) {
+		return new Response(JSON.stringify({ error: 'Room not found' }), {
+			headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+			status: 404
+		});
+	}
 
 	return new Response(JSON.stringify({ _: true }), {
 		headers: { ...corsHeaders, 'Content-Type': 'application/json' },
@@ -37,7 +49,11 @@ async function updateRoom(supabaseClient: SupabaseClient, id: string) {
 		.from('rooms')
 		.update({ is_active: false })
 		.eq('room_id', id);
-	if (error) throw error;
+	if (error)
+		return new Response(JSON.stringify({ error: error.message }), {
+			headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+			status: 500
+		});
 
 	return new Response(JSON.stringify({ _: true }), {
 		headers: { ...corsHeaders, 'Content-Type': 'application/json' },
@@ -51,7 +67,11 @@ async function createRoom(supabaseClient: SupabaseClient) {
 		.from('rooms')
 		.insert([{ room_id }])
 		.select();
-	if (error) throw error;
+	if (error)
+		return new Response(JSON.stringify({ error: error.message }), {
+			headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+			status: 500
+		});
 
 	return new Response(JSON.stringify({ data: data[0] }), {
 		headers: { ...corsHeaders, 'Content-Type': 'application/json' },
@@ -60,56 +80,80 @@ async function createRoom(supabaseClient: SupabaseClient) {
 }
 
 Deno.serve(async (req) => {
-	const { url, method } = req;
+	const { url, method, headers } = req;
 
 	// This is needed if you're planning to invoke your function from a browser.
 	if (method === 'OPTIONS') {
-		return new Response('ok', { headers: corsHeaders });
+		return new Response('OK', { headers: corsHeaders });
 	}
 
-	try {
-		// Create a Supabase client with the Auth context of the logged in user.
-		const supabaseClient = createClient(
-			// Supabase API URL - env var exported by default.
-			Deno.env.get('SUPABASE_URL') ?? '',
-			// Supabase API ANON KEY - env var exported by default.
-			Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-			// Create client with Auth context of the user that called the function.
-			// This way your row-level-security (RLS) policies are applied.
-			{
-				global: {
-					headers: { Authorization: req.headers.get('Authorization')! }
-				}
+	const error = await verifyUnkeyApi(headers);
+	if (error) return error;
+
+	// Create a Supabase client with the Auth context of the logged in user.
+	const supabaseClient = createClient(
+		// Supabase API URL - env var exported by default.
+		Deno.env.get('SUPABASE_URL') ?? '',
+		// Supabase API ANON KEY - env var exported by default.
+		Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+		// Create client with Auth context of the user that called the function.
+		// This way your row-level-security (RLS) policies are applied.
+		{
+			global: {
+				headers: { Authorization: req.headers.get('Authorization')! }
 			}
-		);
-
-		const taskPattern = new URLPattern({ pathname: '/room/:id' });
-		const matchingPath = taskPattern.exec(url);
-		const id = matchingPath ? matchingPath.pathname.groups.id : null;
-
-		// let data = null;
-		// if (method === 'POST' || method === 'PUT') {
-		// 	const body = await req.json();
-		// 	data = body.data;
-		// }
-
-		// call relevant method based on method and id
-		switch (true) {
-			case id && method === 'GET':
-				return getRoom(supabaseClient, id as string);
-			case id && method === 'PUT':
-				return updateRoom(supabaseClient, id as string);
-			case method === 'POST':
-				return createRoom(supabaseClient);
-			default:
-				return new Response('Not Found', { status: 404 });
 		}
-	} catch (error) {
-		console.error(error);
+	);
 
-		return new Response(JSON.stringify({ error: error.message }), {
-			headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-			status: 400
-		});
+	const taskPattern = new URLPattern({ pathname: '/room/:id' });
+	const matchingPath = taskPattern.exec(url);
+	const id = matchingPath ? matchingPath.pathname.groups.id : null;
+
+	// let data = null;
+	// if (method === 'POST' || method === 'PUT') {
+	// 	const body = await req.json();
+	// 	data = body.data;
+	// }
+
+	// call relevant method based on method and id
+	switch (true) {
+		case id && method === 'GET':
+			return getRoom(supabaseClient, id as string);
+		case id && method === 'PUT':
+			return updateRoom(supabaseClient, id as string);
+		case method === 'POST':
+			return createRoom(supabaseClient);
+		default:
+			return new Response(JSON.stringify({ message: 'Not Found' }), {
+				headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+				status: 404
+			});
 	}
 });
+
+async function verifyUnkeyApi(headers: Headers) {
+	const key = headers.get('x-api-key') as string;
+	if (!key)
+		return new Response(JSON.stringify({ message: 'Unauthorized Access' }), {
+			headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+			status: 401
+		});
+	const { result, error } = await verifyKey(key);
+	if (error) {
+		return new Response(
+			JSON.stringify({ message: 'Error verifying API key' }),
+			{
+				headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+				status: 500
+			}
+		);
+	}
+	if (!result.valid) {
+		return new Response(JSON.stringify({ message: 'Unauthorized Access' }), {
+			headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+			status: 401
+		});
+	}
+
+	return null;
+}
